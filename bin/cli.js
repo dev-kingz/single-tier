d1d34ab291683eb9cfe47eb9a178c158d2421970
Saad Khan <saadkhaliq15@gmail.com>
2024-07-29 21:06:41 +0500

    chore: Add ora package for loading spinner in CLI script

--- bin/cli.js
+++ bin/cli.js
@@ -2,7 +2,14 @@
 
 const { execSync } = require('child_process');
 const figlet = require('figlet');
+const ora = require('ora');
 const args = process.argv.slice(2);
+
+// Function to load chalk dynamically
+async function loadChalk() {
+    const chalk = await import('chalk');
+    return chalk.default;
+}
 
 if (args.length !== 1) {
     console.error('Please provide a project name.');
@@ -11,12 +18,6 @@ if (args.length !== 1) {
 
 const projectName = args[0];
 const repoUrl = 'https://github.com/dev-kingz/single-tier.git';
-
-// Function to load chalk dynamically
-async function loadChalk() {
-    const chalk = await import('chalk');
-    return chalk.default;
-}
 
 (async () => {
     const chalk = await loadChalk();
@@ -27,15 +28,24 @@ async function loadChalk() {
         )
     );
 
-    console.log(chalk.green('Creating your new single-tier app...'));
+    console.log(chalk.green('Creating your new single-tier app...\n'));
+
+    const spinner = ora('Cloning repository...').start();
 
     try {
         execSync(`git clone ${repoUrl} ${projectName}`, { stdio: 'inherit' });
+        spinner.succeed('Repository cloned successfully.');
+
         console.log(
             chalk.blueBright(`\nDevKingz: Your ${chalk.yellow(projectName)} app has been created successfully!`),
-            chalk.green("\n\nRun 'npm i' and 'npm run dev' to start developing!")
+            chalk.green("\n\nRun the following commands to start developing:\n"),
+            chalk.cyan(`cd ${projectName}`),
+            chalk.cyan(`npm install`),
+            chalk.cyan(`npm run dev`),
+            chalk.green("\nHappy coding!\n")
         );
     } catch (error) {
+        spinner.fail('Failed to clone repository.');
         console.error(
             chalk.red(`\nDevKingz: :( Failed to clone your new single-tier app: ${error.message}`)
         );
