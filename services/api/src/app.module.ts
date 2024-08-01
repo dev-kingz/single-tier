ba855bf3eae05c804fd41ca259c263ec8fc3c3ef
Saad Khan <saadkhaliq15@gmail.com>
2024-08-02 03:18:12 +0500

    refactor: Remove commented out code for database module

--- services/api/src/app.module.ts
+++ services/api/src/app.module.ts
@@ -2,14 +2,14 @@ import {Module} from "@nestjs/common";
 import {AppController} from "./app.controller";
 import {AppService} from "./app.service";
 import {ConfigModule} from "@nestjs/config";
-import {DatabaseModule} from "./database/database.module";
+// import {DatabaseModule} from "./database/database.module";
 import {ModelsModule} from "./models/models.module";
 import {AuthenticationModule} from "./authentication/authentication.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot({isGlobal: true}),
-    DatabaseModule,
+    // DatabaseModule,
     ModelsModule,
     // <------------ APP MODULES ------------>
     AuthenticationModule,
--- services/api/src/database/database.module.ts
+++ services/api/src/database/database.module.ts
@@ -1,14 +1,14 @@
-import {Global, Module} from "@nestjs/common";
-import {DatabaseConnectionService} from "./services";
-import {MongooseModule} from "@nestjs/mongoose";
+// import {Global, Module} from "@nestjs/common";
+// import {DatabaseConnectionService} from "./services";
+// import {MongooseModule} from "@nestjs/mongoose";
 
-@Global()
-@Module({
-  imports: [
-    MongooseModule.forRootAsync({
-      useClass: DatabaseConnectionService,
-    }),
-  ],
-  providers: [MongooseModule],
-})
-export class DatabaseModule {}
+// @Global()
+// @Module({
+//   imports: [
+//     MongooseModule.forRootAsync({
+//       useClass: DatabaseConnectionService,
+//     }),
+//   ],
+//   providers: [MongooseModule],
+// })
+// export class DatabaseModule {}
