bde7c3d12c044b70d2a9bf3dabf639d4ab1a195e
Saad Khan <saadkhaliq15@gmail.com>
2024-08-02 17:11:55 +0500

    Fixed different ports for different environments

--- services/api/src/app.module.ts
+++ services/api/src/app.module.ts
@@ -8,7 +8,9 @@ import {AuthenticationModule} from "./authentication/authentication.module";
 
 @Module({
   imports: [
-    ConfigModule.forRoot({isGlobal: true}),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     // DatabaseModule,
     // ModelsModule,
     // <------------ APP MODULES ------------>
--- services/api/src/main.ts
+++ services/api/src/main.ts
@@ -3,6 +3,12 @@ import {AppModule} from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const port = process.env.NODE_ENV === "production" ? 4000 : 4001;
+  const appName = process.env.APP_NAME;
+
+  // Use the retrieved configuration values
+  await app.listen(port);
+  console.log(`${appName} is running on: ${await app.getUrl()}`);
 }
 bootstrap();
