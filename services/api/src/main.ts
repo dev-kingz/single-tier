b29b48db49e9fb04d6c1ca9e13691884f94b6796
Saad Khan <saadkhaliq15@gmail.com>
2024-08-03 00:36:52 +0500

    All set for the monorepo monolithic architecture

--- services/api/src/main.ts
+++ services/api/src/main.ts
@@ -3,13 +3,14 @@ import {AppModule} from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   // Retrieve the configuration values
+  const base_url = process.env.BASE_URL;
   const port = process.env.NODE_ENV === "production" ? 4000 : 4001;
   const appName = process.env.APP_NAME;
 
   // Use the retrieved configuration values
   await app.listen(port);
-  console.log(`\n${appName} is running on: ${await app.getUrl()}`);
+  console.log(`\n${appName} is running on: ${base_url}:${port}\n`);
 }
 bootstrap();
