22c1076400629b7dd897dd8e1738c1b78c89a176
Saad Khan <saadkhaliq15@gmail.com>
2024-08-02 19:08:30 +0500

    chore: Update start and build scripts in package.json

--- services/api/src/app.service.ts
+++ services/api/src/app.service.ts
@@ -1,11 +1,9 @@
 import {Injectable} from "@nestjs/common";
-import {ConfigService} from "@nestjs/config";
 
 @Injectable()
 export class AppService {
-  constructor(private configService: ConfigService) {}
 
   getHello(): string {
-    return `Hello World! ${this.configService.get<string>("HELLO")}`;
+    return `Hello World!`;
   }
 }
