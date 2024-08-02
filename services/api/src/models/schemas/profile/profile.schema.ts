2af8785666459b4655b631556a23e76c57fafdf0
Saad Khan <saadkhaliq15@gmail.com>
2024-08-02 23:17:23 +0500

    chore: Enable database module and models in app.module.ts

--- services/api/src/models/schemas/profile/profile.schema.ts
+++ services/api/src/models/schemas/profile/profile.schema.ts
@@ -1,20 +1,20 @@
-// import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
-// import {Types} from "mongoose";
-// import {PersonalInfo, PERSONALINFO_MODEL} from "../personalInfo/personalInfo.schema";
+import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
+import {Types} from "mongoose";
+import {PersonalInfo, PERSONALINFO_MODEL} from "../personalInfo/personalInfo.schema";
 
-// @Schema({
-//   timestamps: true,
-// })
-// export class Profile {
-//   @Prop({required: true, unique: true})
-//   username: string;
+@Schema({
+  timestamps: true,
+})
+export class Profile {
+  @Prop({required: true, unique: true})
+  username: string;
 
-//   @Prop({type: Types.ObjectId, ref: PERSONALINFO_MODEL, required: true})
-//   personalInfo: Types.ObjectId | PersonalInfo;
-// }
+  @Prop({type: Types.ObjectId, ref: PERSONALINFO_MODEL, required: true})
+  personalInfo: Types.ObjectId | PersonalInfo;
+}
 
-// export type ProfileDocument = Profile & Document;
+export type ProfileDocument = Profile & Document;
 
-// export const PROFILE_MODEL = Profile.name; // Profile
+export const PROFILE_MODEL = Profile.name; // Profile
 
-// export const ProfileSchema = SchemaFactory.createForClass(Profile);
+export const ProfileSchema = SchemaFactory.createForClass(Profile);
