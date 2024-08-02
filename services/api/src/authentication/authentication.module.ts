def0d8f7a4cd8ce255f4cad71ad0716bb98ccb69
Saad Khan <saadkhaliq15@gmail.com>
2024-08-03 01:18:58 +0500

    feat: Add AuthenticationController to handle authentication requests

--- services/api/src/authentication/authentication.module.ts
+++ services/api/src/authentication/authentication.module.ts
@@ -1,7 +1,9 @@
 import {Module} from "@nestjs/common";
 import {RegistrationModule} from "./registration/registration.module";
+import { AuthenticationController } from './authentication.controller';
 
 @Module({
   imports: [RegistrationModule],
+  controllers: [AuthenticationController],
 })
 export class AuthenticationModule {}
--- services/api/src/authentication/registration/controllers/registration.controller.ts
+++ services/api/src/authentication/registration/controllers/registration.controller.ts
@@ -1,4 +1,9 @@
-import {Controller} from "@nestjs/common";
+import {Controller, Get} from "@nestjs/common";
 
 @Controller("registration")
-export class RegistrationController {}
+export class RegistrationController {
+  @Get()
+  getHello(): string {
+    return "Hello from Registration Controller";
+  }
+}
