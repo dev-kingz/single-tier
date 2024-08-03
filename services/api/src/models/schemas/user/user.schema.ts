29ce7fcc77796055b56c78be20a01f1e4ac21d42
Saad Khan <saadkhaliq15@gmail.com>
2024-08-03 21:26:32 +0500

    feat: Hash user password before saving in UserSchema

--- services/api/src/models/schemas/user/user.schema.ts
+++ services/api/src/models/schemas/user/user.schema.ts
@@ -1,5 +1,6 @@
 import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
 import {Types} from "mongoose";
+import {hash} from "bcrypt";
 
 @Schema({
   timestamps: true,
@@ -7,10 +8,10 @@ import {Types} from "mongoose";
 export class User {
   @Prop({required: true})
   name: string;
-  
+
   @Prop({required: true, unique: true})
   username: string;
-  
+
   @Prop({required: true, unique: true})
   email: string;
 
@@ -24,3 +25,8 @@ export const USER_MODEL = User.name; // User
 
 export const UserSchema = SchemaFactory.createForClass(User);
 
+UserSchema.pre("save", async function (next) {
+  const hashedPassword = await hash(this.password, 10);
+  this.password = hashedPassword;
+  next();
+});
