7148beb35da2ea75ee4cf1958bf162c21adcf338
Saad Khan <saadkhaliq15@gmail.com>
2024-08-03 22:28:24 +0500

    chore: Add JwtGuard to ProfileController's getProfile endpoint

--- services/api/src/crm/profile/profile.controller.ts
+++ services/api/src/crm/profile/profile.controller.ts
@@ -1,11 +1,13 @@
-import {Controller, Get, Param} from "@nestjs/common";
+import {Controller, Get, Param, UseGuards} from "@nestjs/common";
 import {ProfileService} from "./profile.service";
 import { Types } from "mongoose";
+import { JwtGuard } from "src/auth/authenticator/guards/jwt.guard";
 
 @Controller("/crm/profile")
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
 
+  @UseGuards(JwtGuard)
   @Get(":id")
   async getProfile(@Param("id") id: Types.ObjectId) {
     return this.profileService.getProfile(id);
