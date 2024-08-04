9ec63efa774c046ac5044b4f273d5bf9b5a73222
Saad Khan <saadkhaliq15@gmail.com>
2024-08-05 00:28:57 +0500

    Refactor AuthModal component to use "login" and "signup" actions

--- apps/web/src/auth.ts
+++ apps/web/src/auth.ts
@@ -1,5 +1,34 @@
 import NextAuth from "next-auth";
 
+
 export const {handlers, signIn, signOut, auth} = NextAuth({
-  providers: [],
+  providers: [
+    Credentials({
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        let user = null
+ 
+        // logic to salt and hash password
+        const pwHash = saltAndHashPassword(credentials.password)
+ 
+        // logic to verify if the user exists
+        user = await getUserFromDb(credentials.email, pwHash)
+ 
+        if (!user) {
+          // No user found, so this is their first attempt to login
+          // meaning this is also the place you could do registration
+          throw new Error("User not found.")
+        }
+ 
+        // return user object with their profile data
+        return user
+      },
+    }),
+
+  ],
 });
